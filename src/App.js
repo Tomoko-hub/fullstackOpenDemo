ca1f40c4d02da0b5f633d49948f1a38899ac7099
Tomoko <haisaitomoco@gmail.com>
2021-09-15 15:12:29 +0300

    demo valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Hello = (props) => {
+  return(
+    <div>
+      <p>Hello {props.name}</p>
     </div>
-  );
+  )
+}
+
+const Footer = () => {
+  return(
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
+const App= () => {
+
+  /* const now = new Date()
+  const a = 10
+  const b = 20 */
+
+  return (
+  <>
+    <h1>Greetings</h1>
+    <Hello />
+    <Footer />
+  </>
+  )
 }
 
 export default App;
